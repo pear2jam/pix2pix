fdbcf49178eb030de44ef2b27662be6c0455a848
pear2jam <pear2jam@gmail.com>
2021-11-17 20:40:56 +0300

    wtf

--- tools/data_preparation.py
+++ tools/data_preparation.py
@@ -1,50 +1,80 @@
 """
 Подготовка данных к обучению
 """
-
 import torch
+import torch.nn.functional as tf
+import warnings
+from tools.misc import get_rotate_matrix
 
-"""
-Разделение данных на X и y 
+def split(x, turned_add=False, rotate_add=True, max_angle=0.5, part=1, info=True):
+    """
+    splitting and adding variety to the dataset
+    :param x: input data with shape (len(x), 3, 256, 512)
+    :param turned_add: add flipped images to the end
+    :param rotate_add: add rotated images to the end
+    :param max_angle: maximum absolute angle of rotating for "rotate_add"
+    :param part: part of x to use (float from 0 to 1)
+    :param info: print information about preparing process
 
-enter: (len, 3, 256, 512)
-out:   (len, 2, 3, 256, 256)
-turned_add - удвоить датасет путем добавления в конец отзеркаленные картинки
-"""
+    :raises ValueError: if part < 0 or part > 1
 
+    :return: images data with shape (n*len(x), 2, 3, 256, 256)
+    """
+    if part < 0 or part > 1:
+        raise ValueError("'part' parameter should be float value from 0 to 1")
 
-def split(x, turned_add=True, info=False):
-    x_data = torch.FloatTensor()
+    size = 1
     if turned_add:
-         x_data = torch.FloatTensor(2*len(x), 2, 3, 256, 256)
-    else:
-        x_data = torch.FloatTensor(len(x), 2, 3, 256, 256)
+        size += 1
+    if rotate_add:
+        size += 1
+
+    x_data = torch.FloatTensor(int(size*len(x)*part), 2, 3, 256, 256)
+
+    part_len = int(len(x)*part / 20)  # отображение по 5 процентов
+    total = -1  # текущие 5 процентов
     if info:
-        l = int(len(x_data) / 20)
-        total = -1
-        print("%: ", end="")
-    for i in range(len(x)):
-        if info and i // l > total:
-            print(i // l * 5, end=" ")
-            total = i // l
+        print("preparing data %: ", end="")
+    for i in range(int(len(x)*part)):
+        if info and i // part_len > total:  # обновление процентов
+            print(i // part_len * 5, end=" ")
+            total = i // part_len
+
         x_temp = x[i][0].unfold(2, 256, 256).permute(2, 0, 1, 3)
         x_data[i][0] = x_temp[0]
         x_data[i][1] = x_temp[1]
+
+        shift = 1  # коэфициэнт свдига по длине входного массива для дополнительных обработок
+
         if turned_add:
-            x_data[len(x) + i][0] = torch.flip(x_temp[0], [2])
-            x_data[len(x) + i][1] = torch.flip(x_temp[1], [2])
+            x_data[int(len(x)*part)*shift + i][0] = torch.flip(x_temp[0], [2])
+            x_data[int(len(x)*part)*shift + i][1] = torch.flip(x_temp[1], [2])
+            shift += 1
+
+        if rotate_add:
+            rot_mat = get_rotate_matrix(torch.rand(1, dtype=torch.float)*2*max_angle - max_angle)
+            rot_mat = rot_mat.view(1, 2, 3)
+            warnings.filterwarnings("ignore")
+            grid = tf.affine_grid(rot_mat, [1, 3, 256, 256])
+
+            rot_x = tf.grid_sample(x_temp[0].view(1, 3, 256, 256), grid, padding_mode='reflection')
+            x_data[int(len(x) * part) * shift + i][0] = rot_x[0]
+            rot_x = tf.grid_sample(x_temp[1].resize(1, 3, 256, 256), grid, padding_mode='reflection')
+            x_data[int(len(x) * part) * shift + i][1] = rot_x[0]
 
     if info:
-        print("\nfinished")
+        print("100\nfinished")
+        print("images total:", int(size*len(x)*part))
     return x_data
 
 
-"""
-Перемещение данных на устройство
-"""
-
-
 def move_to(data, device):
+    """
+    moving data to device
+    :param data: data to move
+    :param device: device
+    :return: moved data
+    """
     if isinstance(data, (list, tuple)):
         return [move_to(x, device) for x in data]
     return data.to(device, non_blocking=True)
